5a8466a39b32b3a07ae9e896d2a6b60fcb352484
Iaroslav Krasnokutskii <Yroslavkr1995@gmail.com>
2020-08-19 09:22:39 +0300

    Initial Commit

---
+++ GITHUBCHECK/GITHUBCHECKApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHUBCHECKApp.swift
+//  GITHUBCHECK
+//
+//  Created by Ярослав on 8/19/20.
+//
+
+import SwiftUI
+
+@main
+struct GITHUBCHECKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
